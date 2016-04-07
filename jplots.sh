b87a849179b6d6974784902c89f3c9b5df09c792
James Robertson <jsr@jsrlabs.com>
2016-04-06 20:52:56 -0700

    tweak scripts

--- jplots.sh
+++ jplots.sh
@@ -7,5 +7,6 @@ fi
 
 src/JSBSim scripts/PBScript.xml --outputlogfile=aircraft/$1/$1.csv --aircraft=$1 --initfile=LC39
 
-src/utilities/prep_plot aircraft/$1/$1.csv --plot=data_plot/orbit.xml  | gnuplot | ps2pdf - aircraft/$1/$1.pdf && evince aircraft/$1/$1.pdf &
+src/utilities/prep_plot aircraft/$1/$1.csv --plot=data_plot/orbit.xml  | gnuplot | ps2pdf - aircraft/$1/$1.pdf
+evince aircraft/$1/$1.pdf &
 rm aircraft/$1/$1.csv
